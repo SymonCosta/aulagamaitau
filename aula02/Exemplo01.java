f22eb1a383b027d0028be28a49ad697bd9cf0a45
Symon Costa <costasymon37@gmail.com>
2023-02-25 14:19:46 -0300

    exemplos

---
+++ aula02/Exemplo01.java
@@ -0,0 +1,13 @@
+
+
+public class Exemplo01 {
+
+  public static void main(String[] args) {
+    System.out.println("Ola Mundo!");
+
+    
+
+  }  
+
+    
+}
---
+++ aula02/Exemplo02.java
@@ -0,0 +1,9 @@
+public class Exemplo02 {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("2 + 5=");
+        System.out.println(2+5);
+
+    }
+}
