ac52cb6f27c9fcc7a86183606a10078bc26c0e2a
Symon Costa <costasymon37@gmail.com>
2023-02-27 16:05:23 -0300

    aula03

---
+++ aula03/exercicios/Exercicio01.java
@@ -0,0 +1,36 @@
+package exercicios;
+
+import java.util.Scanner;
+
+
+public class Exercicio01 {
+
+    public static void main(String[] args) {
+    int numero;
+
+    Scanner teclado = new Scanner(System.in);
+
+    System.out.println("Digite o numero: ");
+    numero =  teclado.nextInt();
+
+    if (numero < 20) {
+        System.out.println("Numero digitado: " + numero);
+
+
+    } else  System.out.println("Resultado: " + numero / 2.0); {
+        
+    }
+
+
+
+
+
+
+        
+    }
+    
+
+
+
+
+}
---
+++ aula03/exercicios/Exercicio02.java
@@ -0,0 +1,36 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio02 {
+    public static void main(String[] args) {
+    Scanner teclado = new Scanner(System.in);
+    
+    String nome;
+    double nota1, nota2, media;
+
+    System.out.print("Digite seu nome: ");
+    nome = teclado.nextLine();
+    System.out.print("Digite a nota1: ");
+    nota1 = teclado.nextDouble();
+    System.out.print("Digite a nota2: ");
+    nota2 = teclado.nextDouble();
+
+    media = (nota1*0.4) + (nota2*0.6) /2;
+    System.out.println("Media: " + media);
+
+    if (media >= 6) {
+        System.out.println("Aprovado" );
+        
+    } else {
+        System.out.println("Reprovado" );
+        
+    }
+    
+
+        
+
+
+   teclado.close();
+    }
+}
