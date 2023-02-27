1ee8cac4d89f97971d101b3054c28e62d1bd2d1d
Symon Costa <costasymon37@gmail.com>
2023-02-27 16:47:46 -0300

    aula03

--- aula03/exercicios/Exercicio02.java
+++ aula03/exercicios/Exercicio02.java
@@ -28,9 +28,6 @@ public class Exercicio02 {
     }
     
 
-        
-
-
    teclado.close();
     }
 }
---
+++ aula03/exercicios/Exercicio04.java
@@ -0,0 +1,30 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio04 {
+
+    public static void main(String[] args) {
+    
+    Scanner teclado = new Scanner(System.in);
+    double salarioBruto, valorPrestacao;
+
+    System.out.println("Digite salario Bruto: ");
+    salarioBruto = teclado.nextDouble();
+
+    System.out.println("Digite a prestacao: ");
+    valorPrestacao = teclado.nextDouble();
+
+
+    if (valorPrestacao <= 0.3 * salarioBruto) {
+        System.out.println("Concedido");
+        
+    } else {
+        System.out.println("Negado");
+        
+    }
+
+    teclado.close();
+
+    }
+}
