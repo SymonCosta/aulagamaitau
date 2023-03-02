80a2bbb52f224c6c6355100e9a60c5dded962b49
Symon Costa <costasymon37@gmail.com>
2023-03-02 15:57:05 -0300

    aula04

--- aula04/exercicios/exercicio04/Exercicio04.java
+++ aula04/exercicios/exercicio04/Exercicio04.java
@@ -1,8 +1,8 @@
 package exercicios.exercicio04;
 
 public class Exercicio04 {
-    public  static  void  principal ( String [] args ) {
-        Relogio relogio = new Relogio ( 0 , 0 , 0 );
+    public static void main( String [] args ) {
+        Relogio relogio = new Relogio ( 15 , 51 , 30 );
 
         System.out.println( "Hora: " + relogio . getHora());
         System.out.println( "Minuto: " + relogio . getMinuto());
