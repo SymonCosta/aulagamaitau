c2de189d8c8f9d6daf773826ce80a76f3f460a9e
Symon Costa <costasymon37@gmail.com>
2023-03-10 16:42:20 -0300

    ExercicioTeste01

---
+++ aula14/exercicio01/processador/src/main/java/br/gama/itau/ProcessaNumeros.java
@@ -0,0 +1,39 @@
+package br.gama.itau;
+import java.util.ArrayList;
+import java.util.Collections;
+public class ProcessaNumeros {
+    public int getMinimo(ArrayList<Integer> lista) {
+        // int minimo = lista.get(0);
+
+        // for (Integer numero : lista) {
+        //     if(numero < minimo) {
+        //         minimo = numero;
+        //     }
+        // }
+        // return minimo;
+
+        return Collections.min(lista);
+    }
+
+    public int getMaximo(ArrayList<Integer> lista) {
+        return Collections.max(lista);
+    }
+
+    public int getSize(ArrayList<Integer> lista) {
+        return lista.size();
+    }
+
+    public double getMedia(ArrayList<Integer> lista) {
+        double media = 0;
+
+        // soma todos os valores
+        for (Integer numero : lista) {
+            media += numero; // media = media + numero;
+        }
+        // divide pela quantidade de valores
+        // media = media / lista.size();
+        media /= lista.size();
+
+        return media;
+    }
+}
