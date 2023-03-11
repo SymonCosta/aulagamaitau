45ed875f5f13ec315fe4cf5df8507293985c8737
Symon Costa <costasymon37@gmail.com>
2023-03-10 22:23:37 -0300

    Spring01

---
+++ aula14/spring01/src/main/java/br/gama/itau/spring01/controller/Hello.java
@@ -0,0 +1,16 @@
+package br.gama.itau.spring01.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+    
+    @GetMapping
+    public String hello() {
+        return "Ola mundo!";
+
+    }
+
+
+}
