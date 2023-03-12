f8c2b15266a283d6dd60dbed6d34afd53deac913
Symon Costa <costasymon37@gmail.com>
2023-03-11 23:04:03 -0300

    Spring02

--- aula15/spring02/src/main/java/br/gama/itau/spring02/controller/ProfessorController.java
+++ aula15/spring02/src/main/java/br/gama/itau/spring02/controller/ProfessorController.java
@@ -3,18 +3,23 @@ package br.gama.itau.spring02.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import br.gama.itau.spring02.model.Professor;
 import br.gama.itau.spring02.repository.ProfessorRepo;
 
 @RestController //indica que esta classe é um controller do tipo REST
+@RequestMapping("/professor") // mapeamento deta classe
 public class ProfessorController {
 
+    // gera a implementacao da clase e um objeto
     @Autowired
     private ProfessorRepo repo;
-    @GetMapping
-    public Professor getProfessor() {
-        return repo.findById(1L).get();
+
+    @GetMapping("/{codigo}")
+    public Professor getProfessor(@PathVariable long codigo) {
+        return repo.findById(codigo).get();
         
     }
 
